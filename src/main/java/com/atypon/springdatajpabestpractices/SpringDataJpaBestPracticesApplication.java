95b2acfec276a4ed914b0d9477d02d121c9801ae
ahomaidat <ahomaidat@atypon.com>
2026-01-15 16:04:47 +0300

    Initial commit

---
+++ src/main/java/com/atypon/springdatajpabestpractices/SpringDataJpaBestPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.atypon.springdatajpabestpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaBestPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaBestPracticesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atypon/springdatajpabestpractices/SpringDataJpaBestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atypon.springdatajpabestpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaBestPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
